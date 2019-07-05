a84d047cb1b4b795ebc4ac15a58179890b0a6f57
Juan Navarro <jnavcamacho@gmail.com>
2019-07-04 23:49:57 -0600

    [Add] Styles usgin grid from ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Item } from  './../../models/Item';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,88 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: Item[];
+  constructor() {
+    this.items = [{
+      title: 'Item 1',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/640x360'
+    },
+    {
+      title: 'Item 2',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/360x360'
+    },
+    {
+      title: 'Item 3',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/540x360'
+    },
+    {
+      title: 'Item 4',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/680x400'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/320x180'
+    },
+    {
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/320x180'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/320x180'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/320x180'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/860x440'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/220x80'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/320x180'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/720x360'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/300x160'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/720x580'
+    },{
+      title: 'Item 5',
+      subtitle: 'Subtitle',
+      description: 'Description',
+      imageURL: 'http://via.placeholder.com/220x180'
+    }];
+  }
 
 }
---
+++ src/models/Item.ts
@@ -0,0 +1,7 @@
+export class Item {
+    title: String;
+    subtitle: String;
+    description: String;
+    imageURL: String;
+  }
+
